5fd137a36b5c161b64ea0d37d957cf1cc4f98474
Bruno de Jesus <bruno.jesus.carmo@gmail.com>
2024-03-09 21:57:18 -0300

    Corrigir importação de componente Search

--- components/shared/collection.tsx
+++ components/shared/collection.tsx
@@ -17,7 +17,7 @@ import { formUrlQuery } from "@/lib/utils";
 
 import { Button } from "../ui/button";
 
-import { Search } from "./Search";
+import { Search } from "./search";
 
 export const Collection = ({
   hasSearch = false,
